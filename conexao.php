046ae3bdba2aa166f28a3584cdcc50858f47eade
GIRLENE MARIA PEREIRA <103802471+Girlene22@users.noreply.github.com>
2023-05-23 11:28:02 -0300

    Update 23/05/2023

--- conexao.php
+++ conexao.php
@@ -4,9 +4,9 @@ $usuario = 'root';
 $senha = '';
 $database = 'login';
 $host = 'localhost';
-
+// Create connection
 $mysqli = new mysqli($host, $usuario, $senha, $database);
-
+// Check connection
 if($mysqli->error) {
     die("Falha ao conectar ao banco de dados: " . $mysqli->error);
 }
